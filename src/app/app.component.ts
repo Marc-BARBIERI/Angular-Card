2eed37a1b43c52b25c46d8626be26afae667ec7f
Marc-BARBIERI <barbierimarc84@gmail.com>
2025-03-30 19:26:44 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from "@angular/core";
+import type { Pokemon, PokemonList } from "./pokemon.model";
+import { POKEMON_LIST } from "./pokemon-list";
 
 @Component({
-  selector: 'app-root',
-  imports: [],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+	selector: "app-root",
+	imports: [],
+	templateUrl: "./app.component.html",
+	styleUrl: "./app.component.css",
 })
 export class AppComponent {
-  title = 'angular-card-app';
+	pokemonList = signal(POKEMON_LIST);
+	name = signal("pikachu");
+	life = signal(25);
+
+	size(pokemon: Pokemon) {
+		if (pokemon.life < 15) {
+			return "small";
+		}
+		if (pokemon.life > 25) {
+			return "large";
+		}
+		return "medium";
+	}
+
+	imageSrc = signal(
+		"https://assets.pokemon.com/assets/cms2/img/pokedex/detail/025.png",
+	);
+
+	incrementLife(pokemon: Pokemon) {
+		pokemon.life = pokemon.life + 1;
+	}
+	decrementLife(pokemon: Pokemon) {
+		pokemon.life = pokemon.life - 1;
+	}
 }
