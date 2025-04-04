d74a5ac06aab6edba6e652430d888cd8eb81f5e1
Marc-BARBIERI <barbierimarc84@gmail.com>
2025-04-04 22:59:33 +0200

    feat: create component pokemonEdit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,8 +5,14 @@ import type { ApplicationConfig } from "@angular/core";
 import { PokemonListComponent } from "./pokemon/pokemon-list/pokemon-list.component";
 import { PokemonProfileComponent } from "./pokemon/pokemon-profile/pokemon-profile.component";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+import { PokemonEditComponent } from "./pokemon/pokemon-edit/pokemon-edit.component";
 
 const routes: Routes = [
+	{
+		path: "pokemons/edit/:id",
+		component: PokemonEditComponent,
+		title: "Edition d'un pokemon",
+	},
 	{
 		path: "pokemons/:id",
 		component: PokemonProfileComponent,
