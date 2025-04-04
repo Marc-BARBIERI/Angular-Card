381052cd576bebc7df60b5e7c66838544c2e2c82
Marc-BARBIERI <barbierimarc84@gmail.com>
2025-04-04 23:42:59 +0200

    feat: display form data on submission

--- src/app/pokemon/pokemon-edit/pokemon-edit.component.ts
+++ src/app/pokemon/pokemon-edit/pokemon-edit.component.ts
@@ -2,11 +2,18 @@ import { DatePipe } from "@angular/common";
 import { Component, inject, signal } from "@angular/core";
 import { ActivatedRoute, RouterLink } from "@angular/router";
 import { PokemonService } from "../../pokemon.service";
+import {
+	Form,
+	FormArray,
+	FormControl,
+	FormGroup,
+	ReactiveFormsModule,
+} from "@angular/forms";
 
 @Component({
 	selector: "app-pokemon-edit",
 	standalone: true,
-	imports: [DatePipe, RouterLink],
+	imports: [RouterLink, ReactiveFormsModule],
 	templateUrl: "./pokemon-edit.component.html",
 	styles: "",
 })
@@ -19,4 +26,33 @@ export class PokemonEditComponent {
 	readonly pokemon = signal(
 		this.pokemonService.getPokemonById(this.pokemonId()),
 	).asReadonly();
+
+	readonly form = new FormGroup({
+		name: new FormControl(this.pokemon().name),
+		life: new FormControl(this.pokemon().life),
+		damage: new FormControl(this.pokemon().damage),
+		types: new FormArray(
+			this.pokemon().types.map((type) => new FormControl(type)),
+		),
+	});
+
+	get pokemonTypeList(): FormArray {
+		return this.form.get("types") as FormArray;
+	}
+	isPokemonTypeSelected(type: string): boolean {
+		return !!this.pokemonTypeList.controls.find(
+			(control) => control.value === type,
+		);
+	}
+	onPokemontypeChange(type: string, ischecked: boolean) {
+		if (ischecked) {
+			const control = new FormControl(type);
+			this.pokemonTypeList.push(control);
+		} else {
+			const index = this.pokemonTypeList.controls
+				.map((control) => control.value)
+				.indexOf(type);
+			this.pokemonTypeList.removeAt(index);
+		}
+	}
 }
