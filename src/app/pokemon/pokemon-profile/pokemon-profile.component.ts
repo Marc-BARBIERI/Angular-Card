2dd61c7a07a86833c3d4499f173a06cb222ed384
Marc-BARBIERI <barbierimarc84@gmail.com>
2025-04-18 10:05:31 +0200

    Implement custom error handling for HTTP requests

--- src/app/pokemon/pokemon-profile/pokemon-profile.component.ts
+++ src/app/pokemon/pokemon-profile/pokemon-profile.component.ts
@@ -1,9 +1,10 @@
-import { Component, inject, signal } from "@angular/core";
+import { Component, computed, inject, signal } from "@angular/core";
 import { ActivatedRoute, RouterLink } from "@angular/router";
 import { PokemonService } from "../../pokemon.service";
 import { Pokemon } from "../../pokemon.model";
 import { DatePipe } from "@angular/common";
 import { toSignal } from "@angular/core/rxjs-interop";
+import { catchError, map, of } from "rxjs";
 
 @Component({
 	selector: "app-pokemon-profile",
@@ -14,8 +15,20 @@ import { toSignal } from "@angular/core/rxjs-interop";
 export class PokemonProfileComponent {
 	readonly #route = inject(ActivatedRoute);
 	readonly #pokemonService = inject(PokemonService);
+
 	readonly #pokemonId = Number(this.#route.snapshot.paramMap.get("id"));
-	readonly pokemon = toSignal(
-		this.#pokemonService.getPokemonById(this.#pokemonId),
+
+	readonly #pokemonResponse = toSignal(
+		this.#pokemonService.getPokemonById(this.#pokemonId).pipe(
+			map((pokemon) => ({
+				value: pokemon,
+				error: undefined,
+			})),
+			catchError((error) => of({ value: undefined, error: error })),
+		),
 	);
+
+	readonly loading = computed(() => this.#pokemonResponse === undefined);
+	readonly error = computed(() => this.#pokemonResponse()?.error);
+	readonly pokemon = computed(() => this.#pokemonResponse()?.value);
 }
